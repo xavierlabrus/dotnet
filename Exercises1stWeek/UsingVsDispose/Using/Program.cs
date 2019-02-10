1bb5b22b758cd6992be65d3e341db1adcf05ca3d
xavierlabrus <xevilabrus@gmail.com>
2019-02-10 20:55:20 +0100

    [NEW] Using vs Dispose

---
+++ Exercises1stWeek/UsingVsDispose/Using/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream word = File.Create("test.txt"))
+            {
+                Byte[] palabra = new UTF8Encoding(true).GetBytes("test");
+                word.Write(palabra, 0, palabra.Length);
+
+            }
+        }
+    }
+}
