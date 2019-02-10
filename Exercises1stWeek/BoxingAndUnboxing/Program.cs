7fb819902e77515417cf37b61d9f2fcfaa2ca78c
xavierlabrus <xevilabrus@gmail.com>
2019-02-10 18:33:46 +0100

    [NEW] Exercise Boxing and Unboxing

---
+++ Exercises1stWeek/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+            object ob = number;
+            int second = (int)ob;
+        }
+    }
+}
