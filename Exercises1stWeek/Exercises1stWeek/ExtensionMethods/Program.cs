2f706576c4b3f4dea059356ae7768f6456fe5a02
xavierlabrus <xevilabrus@gmail.com>
2019-02-10 21:18:21 +0100

    [NEW] Create Project and Methods Extension

---
+++ Exercises1stWeek/Exercises1stWeek/ExtensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(5.multiple(3));
+            Console.ReadKey();
+        }
+    }
+
+    static class IntegerExtensiveMethods
+    {
+        public static int multiple(this int valor, int multi)
+        {
+            return valor * multi;
+        }
+    }
+}
