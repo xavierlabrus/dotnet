94642ca5e0f25e70a7ad77b1c80431e6faf1e694
xavierlabrus <xevilabrus@gmail.com>
2019-02-10 21:32:54 +0100

    [NEW] Add code of Using and Dispose

---
+++ Exercises1stWeek/Exercises1stWeek/Dispose/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Dispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream word = File.Create("test2.txt");
+            try
+            {
+                Byte[] palabra = new UTF8Encoding(true).GetBytes("test2");
+                word.Write(palabra, 0, palabra.Length);
+
+            }
+            catch
+            {
+                throw new FileNotFoundException("the file doesn't exists");
+            }
+            finally
+            {
+                word.Dispose();
+            }
+        }
+    }
+}
---
+++ Exercises1stWeek/Exercises1stWeek/Using/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream word = File.Create("test.txt"))
+            {
+                Byte[] palabra = new UTF8Encoding(true).GetBytes("test");
+                word.Write(palabra, 0, palabra.Length);
+
+            }
+        }
+    }
+}
