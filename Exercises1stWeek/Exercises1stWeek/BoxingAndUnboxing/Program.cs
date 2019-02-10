97ea50fb8e16c8e520aa6c5c08c26c1db315c587
xavierlabrus <xevilabrus@gmail.com>
2019-02-10 21:25:23 +0100

    [NEW] Add Code of Boxing and

---
+++ Exercises1stWeek/Exercises1stWeek/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 19;
+            object ob = number;
+            int result = (int)ob;
+        }
+    }
+}
